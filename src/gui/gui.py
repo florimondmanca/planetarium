d6262a727d3c9eba38fcdcde755d123a393efca0
Manca Florimond <florimond.manca@student.ecp.fr>
2017-03-19 19:22:54 +0100

    Add random star generation. Restructure the GUI class.

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -4,12 +4,13 @@ from ..core.simulate import System
 
 
 def screen_coords(vec):
-    return (settings.SCREEN.WIDTH // 2 + int(50 * vec.x),
-            settings.SCREEN.HEIGHT // 2 + int(-50 * vec.y))
+    return (settings.SCREEN.CENTER[0] + int(settings.ZOOM.ZOOM * vec.x),
+            settings.SCREEN.CENTER[1] + int(-settings.ZOOM.ZOOM * vec.y))
 
 
 def screen_radius(body):
-    return int(10 * body.mass ** .1)
+    return int(settings.ZOOM.ZOOM * settings.SCREEN_RADIUS.FACT *
+               body.mass ** settings.SCREEN_RADIUS.EXP)
 
 
 class Gui:
@@ -20,47 +21,81 @@ class Gui:
         self.clock = pygame.time.Clock()
         self.colors = {}
         self.names = {}
+        self.init()
+
+    def init(self):
+        # attach color to body
         for body in self.system.bodies:
             self.colors[body.name] = settings.COLORS.random()
+        # attach name to body
         for body in self.system.bodies:
             name_image = settings.FONT.NAME.render(
                 body.name, 1, self.colors[body.name])
             name_rect = name_image.get_rect()
             self.names[body.name] = (name_image, name_rect)
+        # make some starry background
+        self.background = pygame.Surface(settings.SCREEN.SIZE)
+        for _ in range(settings.BACKGROUND.NSTARS):
+            pygame.draw.rect(self.background,
+                             settings.COLORS.random_gray(),
+                             ((settings.randx(), settings.randy()), (1, 1)))
+        # draw first background
+        self.draw_background()
 
     def update(self):
         self.system.update()
 
+    def draw_background(self):
+        self.screen.fill(settings.COLORS.BLACK)
+        self.screen.blit(self.background, (0, 0))
+
+    def draw_body(self, body):
+        if not settings.DRAW.BODY:
+            return
+        pygame.draw.circle(
+            self.screen,
+            self.colors[body.name],
+            screen_coords(body.pos),
+            screen_radius(body),
+        )
+
+    def draw_vel(self, body):
+        if not settings.DRAW.VEL:
+            return
+        pygame.draw.line(
+            self.screen,
+            settings.COLORS.RED,
+            screen_coords(body.pos),
+            screen_coords(body.pos + settings.ZOOM.VEL * body.vel),
+        )
+
+    def draw_forces(self, body):
+        if not settings.DRAW.FORCES:
+            return
+        pygame.draw.line(
+            self.screen,
+            settings.COLORS.GREEN,
+            screen_coords(body.pos),
+            screen_coords(body.pos + settings.ZOOM.FORCES *
+                          body.prev_forces),
+        )
+
+    def draw_name(self, body):
+        if not settings.DRAW.NAMES:
+            return
+        image, rect = self.names[body.name]
+        rect.center = screen_coords(body.pos)
+        rect.move_ip(0, -screen_radius(body) - 10)
+        self.screen.blit(image, rect)
+
     def draw(self):
-        if settings.SHOW.BACKGROUND:
-            self.screen.fill(settings.COLORS.BLACK)
+        if settings.BACKGROUND.REDRAW:
+            self.draw_background()
         for body in self.system.bodies:
-            if settings.SHOW.BODY:
-                pygame.draw.circle(
-                    self.screen,
-                    self.colors[body.name],
-                    screen_coords(body.pos),
-                    screen_radius(body),
-                )
-            if settings.SHOW.VEL:
-                pygame.draw.line(
-                    self.screen,
-                    settings.COLORS.RED,
-                    screen_coords(body.pos),
-                    screen_coords(body.pos + .1 * body.vel),
-                )
-            if settings.SHOW.FORCES:
-                pygame.draw.line(
-                    self.screen,
-                    settings.COLORS.GREEN,
-                    screen_coords(body.pos),
-                    screen_coords(body.pos + 1000 * body.prev_forces),
-                )
-            if settings.SHOW.NAMES:
-                image, rect = self.names[body.name]
-                rect.center = screen_coords(body.pos)
-                rect.move_ip(0, -screen_radius(body) - 5)
-                self.screen.blit(image, rect)
+            self.draw_body(body)
+            self.draw_vel(body)
+            self.draw_forces(body)
+            self.draw_name(body)
         pygame.display.flip()
 
     def run(self):
--- src/gui/settings.py
+++ src/gui/settings.py
@@ -7,12 +7,15 @@ pygame.font.init()
 class SCREEN:
     WIDTH = 1200
     HEIGHT = 700
+    HALF_WIDTH = WIDTH // 2
+    HALF_HEIGHT = HEIGHT // 2
+    CENTER = (HALF_WIDTH, HALF_HEIGHT)
     SIZE = (WIDTH, HEIGHT)
 
 
 class TIME:
-    FPS = 60
-    UPDATE_PER_FRAME = 4  # effective fps ≈ 1 / (.01 + .012 * this)
+    FPS = 30
+    UPDATE_PER_FRAME = 2  # effective fps ≈ 1 / (.01 + .012 * this)
 
 
 class COLORS:
@@ -28,14 +31,42 @@ class COLORS:
             colorcode += random.choice('0123456789abcdef')
         return pygame.Color(colorcode)
 
+    @staticmethod
+    def random_gray():
+        gray = int(255 * random.random()**3)
+        return pygame.Color(gray, gray, gray)
+
+
+class ZOOM:
+    ZOOM = 20
+    VEL = 0.1
+    FORCES = 1000
+
+
+class SCREEN_RADIUS:
+    FACT = .25
+    EXP = 0.15
+
 
 class FONT:
     NAME = pygame.font.SysFont('arial', 10)
 
 
-class SHOW:
-    BACKGROUND = True
+class BACKGROUND:
+    REDRAW = True
+    NSTARS = int(1e5 / ZOOM.ZOOM)
+
+
+class DRAW:
     BODY = True
     NAMES = True
     VEL = False
     FORCES = False
+
+
+def randx():
+    return int(random.random() * SCREEN.WIDTH)
+
+
+def randy():
+    return int(random.random() * SCREEN.HEIGHT)
